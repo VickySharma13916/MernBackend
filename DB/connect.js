4e67722daffec22320dfa05192d76e28c2dfe9b9
Vicky <vickysharma13916@gmail.com>
2024-01-07 19:47:40 +0530

    Update the MongoDB URL

--- DB/connect.js
+++ DB/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const ConnectDB = (uri) => {
   return mongoose
-    .connect(uri)
+    .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
       console.log("Connect to the Database");
     })
